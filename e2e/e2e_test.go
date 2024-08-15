d9cfdac1fbb4b81fb62e1045fffc96f1abb047fc
Case Wylie <cmwylie19@defenseunicorns.com>
2024-08-15 16:38:55 -0500

    chore: e2e tests
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -2,6 +2,7 @@ package e2e_test
 
 import (
 	"bytes"
+	"os"
 	"os/exec"
 	"time"
 
@@ -25,19 +26,19 @@ var _ = Describe("E2E Test", func() {
 	Context("When deploying the application", func() {
 		It("should deploy successfully and produce logs", func() {
 			deployApplication(kubeConfigPath)
-			// takes 30 seconds for the pods to start being created
-			time.Sleep(30 * time.Second)
+			// 60 seconds for logs to start
+			time.Sleep(60 * time.Second)
 
 			// Check logs
 			podLogs := getPodLogs(kubeConfigPath)
 			Expect(podLogs).To(ContainSubstring("Auditor successfully created pod:"))
-			Expect(podLogs).To(ContainSubstring("Watch Controller successfully deleted pod:"))
+			Expect(podLogs).To(ContainSubstring("Watch Controller failed to deleted pod"))
 		})
 	})
 })
 
 func buildImage() {
-	cmd := exec.Command("docker", "build", "-t", "watch-auditor:dev", ".", "-f", "../Dockerfile.arm")
+	cmd := exec.Command("docker", "build", "-t", "watch-auditor:dev", "..", "-f", "../Dockerfile.arm")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	cmd.Stderr = &out
@@ -70,7 +71,8 @@ func setupKindCluster() string {
 		Expect(err).NotTo(HaveOccurred(), "Failed to create the kind cluster")
 	}
 
-	kubeConfigPath := out.String()
+	// Set kubeConfigPath to the default kubeconfig path used by kind
+	kubeConfigPath := os.Getenv("HOME") + "/.kube/config"
 	return kubeConfigPath
 }
 
@@ -79,13 +81,20 @@ func teardownKindCluster(kubeConfigPath string) {
 	cmd := exec.Command("kind", "delete", "cluster", "--kubeconfig", kubeConfigPath)
 	err := cmd.Run()
 	Expect(err).NotTo(HaveOccurred())
+
 }
 
 func deployApplication(kubeConfigPath string) {
 	// Deploy your application using kubectl or any other method
 	cmd := exec.Command("kubectl", "apply", "-k", "../kustomize/overlays/dev", "--kubeconfig", kubeConfigPath)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &out
 	err := cmd.Run()
-	Expect(err).NotTo(HaveOccurred())
+	if err != nil {
+		GinkgoWriter.Write(out.Bytes())
+		Expect(err).NotTo(HaveOccurred(), "Failed to deploy the application")
+	}
 }
 
 func getPodLogs(kubeConfigPath string) string {
@@ -95,5 +104,6 @@ func getPodLogs(kubeConfigPath string) string {
 	cmd.Stdout = &out
 	err := cmd.Run()
 	Expect(err).NotTo(HaveOccurred())
+	GinkgoWriter.Write(out.Bytes())
 	return out.String()
 }
